3974cd8158a1f9cbd11941c46558b14390cecf03
Fernando Boniotti <fernando.boniotti@gmail.com>
2020-06-02 13:05:58 -0300

    deploy

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,8 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get('/foods');
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -37,7 +38,11 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const response = await api.post('/foods', {
+        ...food,
+        available: true,
+      });
+      setFoods(state => [...state, response.data]);
     } catch (err) {
       console.log(err);
     }
@@ -46,11 +51,27 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const response = await api.put(`/foods/${editingFood.id}`, {
+      ...editingFood,
+      ...food,
+    });
+
+    setFoods(plates => {
+      const newPlate = plates.map(item => {
+        if (item.id === editingFood.id) {
+          return { ...response.data };
+        }
+        return item;
+      });
+      return newPlate;
+    });
+
+    setEditingFood({} as IFoodPlate);
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+    setFoods(state => state.filter(stateFood => stateFood.id !== id));
   }
 
   function toggleModal(): void {
@@ -62,7 +83,7 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
   }
 
   return (
@@ -79,7 +100,6 @@ const Dashboard: React.FC = () => {
         editingFood={editingFood}
         handleUpdateFood={handleUpdateFood}
       />
-
       <FoodsContainer data-testid="foods-list">
         {foods &&
           foods.map(food => (
@@ -88,6 +108,7 @@ const Dashboard: React.FC = () => {
               food={food}
               handleDelete={handleDeleteFood}
               handleEditFood={handleEditFood}
+              openModal={toggleEditModal}
             />
           ))}
       </FoodsContainer>
